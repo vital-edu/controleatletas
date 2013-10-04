1a21eb9e234199389f2bf4f30e71ac314b3bd098
Eduardo Vital <vitaldu@gmail.com>
2013-10-04 03:58:25 -0300

    Resolvendo erro grave que impedia o programa de ser executado

--- ControleAtleta/src/controleatleta/ControleAtleta.java
+++ ControleAtleta/src/controleatleta/ControleAtleta.java
@@ -1,25 +1,9 @@
 package controleatleta;
 
-import javax.swing.JOptionPane;
-
 public class ControleAtleta {
 
     public static void main(String[] args) {
-        String opcao;
-        
-        Menu menuPrincipal = new Menu();
-        menuPrincipal.setVisible(true);
-        
-        opcao = menuPrincipal.getOpcao();
-     
-        if ("Boxeador".equals(opcao)){    
-            CadastroBoxeador cadastro1 = new CadastroBoxeador();
-            cadastro1.setVisible(true);
-        }else if("Judoca".equals(opcao)){
-            CadastroJudoca cadastro2 = new CadastroJudoca();
-            cadastro2.setVisible(true);
-        }else{
-            JOptionPane.showMessageDialog(null,"Opçao Invalida");        
-        }       
+        CadastroBoxeador cadastro = new CadastroBoxeador();
+        cadastro.setVisible(true);
     }
 }
