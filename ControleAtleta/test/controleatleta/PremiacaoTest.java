a316885d2667e3d8e4c178c241ecfc19b106343b
Eduardo Vital <vitaldu@gmail.com>
2013-10-04 01:28:26 -0300

    Adicionando teste automatizado para a classe Premiacao.

---
+++ ControleAtleta/test/controleatleta/PremiacaoTest.java
@@ -0,0 +1,53 @@
+package controleatleta;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PremiacaoTest {
+    
+    Premiacao umaPremiacao;
+    
+    @Before
+    public void setUp() {
+        umaPremiacao = new Premiacao("Campeonato Mundial", 2012,2);
+    }
+
+    @Test
+    public void testGetTitulo() {
+        assertEquals("Campeonato Mundial",umaPremiacao.getTitulo());
+    }
+
+    @Test
+    public void testSetTitulo() {
+        umaPremiacao.setTitulo("Olimpiadas");
+        assertEquals(umaPremiacao.getTitulo(),"Olimpiadas");
+    }
+
+    @Test
+    public void testGetAno() {
+        assertEquals(2012,umaPremiacao.getAno());
+    }
+
+    @Test
+    public void testSetAno() {
+        umaPremiacao.setAno(2013);
+        assertEquals(umaPremiacao.getAno(), 2013);
+    }
+
+    @Test
+    public void testGetColocacao() {
+        assertEquals(2, umaPremiacao.getColocacao());
+    }
+
+    @Test
+    public void testSetColocacao() {
+        umaPremiacao.setColocacao(1);
+        assertEquals(umaPremiacao.getColocacao(), 1);
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("2º lugar - 2012 - Campeonato Mundial", umaPremiacao.toString());
+    }
+}
