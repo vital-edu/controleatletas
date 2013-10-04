23ec235da2afcf3d343a32d1894186abed2afcc0
Eduardo Vital <vitaldu@gmail.com>
2013-10-04 02:59:48 -0300

    Modificando classe principal para o programa executar de acordo com o tipo de atleta que se deseja cadastrar

--- ControleAtleta/src/controleatleta/ControleAtleta.java
+++ ControleAtleta/src/controleatleta/ControleAtleta.java
@@ -1,9 +1,25 @@
 package controleatleta;
 
+import javax.swing.JOptionPane;
+
 public class ControleAtleta {
 
     public static void main(String[] args) {
-        CadastroBoxeador cadastro = new CadastroBoxeador();
-        cadastro.setVisible(true);
+        String opcao;
+        
+        Menu menuPrincipal = new Menu();
+        menuPrincipal.setVisible(true);
+        
+        opcao = menuPrincipal.getOpcao();
+     
+        if ("Boxeador".equals(opcao)){    
+            CadastroBoxeador cadastro1 = new CadastroBoxeador();
+            cadastro1.setVisible(true);
+        }else if("Judoca".equals(opcao)){
+            CadastroJudoca cadastro2 = new CadastroJudoca();
+            cadastro2.setVisible(true);
+        }else{
+            JOptionPane.showMessageDialog(null,"Opçao Invalida");        
+        }       
     }
 }
