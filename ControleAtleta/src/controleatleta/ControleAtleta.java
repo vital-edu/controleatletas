c698ebea968fe7dbfc9bb4c891541cd5dc0220dc
Eduardo Vital <vitaldu@gmail.com>
2013-10-05 12:19:02 -0300

    Modificando classe para que ela apenas chame o Menu.

--- ControleAtleta/src/controleatleta/ControleAtleta.java
+++ ControleAtleta/src/controleatleta/ControleAtleta.java
@@ -3,7 +3,11 @@ package controleatleta;
 public class ControleAtleta {
 
     public static void main(String[] args) {
+        Menu cadastro = new Menu();
+        cadastro.setVisible(true);
+        /*
         CadastroJudoca cadastro = new CadastroJudoca();
         cadastro.setVisible(true);
+        **/
     }
 }
