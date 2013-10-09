fe5a4bd21a79347148d864effa70d909177bbfa2
Eduardo Vital <vitaldu@gmail.com>
2013-10-09 13:35:34 -0300

    Criando teste automatixado para o controlador ControleJudoca.

--- ControleAtleta/src/controleatleta/ControleAtleta.java
+++ ControleAtleta/src/controleatleta/ControleAtleta.java
@@ -5,9 +5,5 @@ public class ControleAtleta {
     public static void main(String[] args) {
         Menu cadastro = new Menu();
         cadastro.setVisible(true);
-        /*
-        CadastroJudoca cadastro = new CadastroJudoca();
-        cadastro.setVisible(true);
-        **/
     }
 }
