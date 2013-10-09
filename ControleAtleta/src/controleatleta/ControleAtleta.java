f71ad23b1004dc4a185d9ec94873344210098566
Eduardo Vital <vitaldu@gmail.com>
2013-10-09 13:54:50 -0300

    Corrigindo erros no código.

--- ControleAtleta/src/controleatleta/ControleAtleta.java
+++ ControleAtleta/src/controleatleta/ControleAtleta.java
@@ -5,5 +5,9 @@ public class ControleAtleta {
     public static void main(String[] args) {
         Menu cadastro = new Menu();
         cadastro.setVisible(true);
+        /*
+        CadastroJudoca cadastro = new CadastroJudoca();
+        cadastro.setVisible(true);
+        **/
     }
 }
